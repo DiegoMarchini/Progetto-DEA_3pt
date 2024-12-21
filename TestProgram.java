1689c976e541b87e5817f774be5de90007826160
DiegoMarchini <131981756+DiegoMarchini@users.noreply.github.com>
2024-12-21 16:26:06 +0100

    copiato tutto dal progetto dei 2 pt tranne insert

--- TestProgram.java
+++ TestProgram.java
@@ -25,16 +25,28 @@ import java.io.BufferedReader;
  class SkipListPQ {
     private double alpha;
     private Random rand;
+     //AGGIUNTE MIE
+     private List<List<MyEntry>> sl;
+     //private static List<MyEntry> empty_list;
+     //FINE AGGIUNTE MIE
     public SkipListPQ(double alpha) {
         this.alpha = alpha;
         this.rand = new Random();
+        this.sl = new ArrayList<>();//col fatto ch è un array che implementa una lista potrebbe dare problemi nell'insert
+        List<MyEntry> empty_list = new ArrayList<MyEntry>();
+        empty_list.add(new MyEntry(Integer.MIN_VALUE,"-inf"));
+        empty_list.add(new MyEntry(Integer.MAX_VALUE,"+inf"));
+        sl.add(empty_list);
  // TO BE COMPLETED       
     }
     public int size() {
- // TO BE COMPLETED       
+        return sl.get(0).size() - 2;
+ // TO BE COMPLETED (finito?)       
     }
     public MyEntry min() {
- // TO BE COMPLETED 
+        if(this.size()!=0)return sl.get(0).get(1);
+        return null;
+ // TO BE COMPLETED (finito?) 
     }
     public int insert(int key, String value) {
  // TO BE COMPLETED 
@@ -55,10 +67,39 @@ import java.io.BufferedReader;
         return level;
     }
     public MyEntry removeMin() {
- // TO BE COMPLETED 
+        MyEntry e = min();
+        if(e==null) return e;
+        int i = 0;
+        boolean check = sl.get(i).remove(e);
+        while(check)
+        {
+            i++;
+            check = sl.get(i).remove(e);  
+        } 
+        //parte di pulizia
+        while (sl.size() > 1 && sl.get(sl.size() - 2).size() == 2) sl.remove(sl.size() - 1);
+        //fine pulizia
+        return e;
+ // TO BE COMPLETED (finito?) 
     }
     public void print() {
- // TO BE COMPLETED 
+        int i = 1;
+        String s = "";
+        while(i < sl.get(0).size()-1)
+        {   
+            MyEntry e = sl.get(0).get(i);
+            int j = 1;
+            while((j < sl.size()) && sl.get(j).contains(e))
+            {
+                j++;
+            } 
+            s+=(e + " "+ j + ", ");
+            i++;
+        }
+        s = s.substring(0,s.length()-2);
+        //System.out.println(sl.get(0).size()-2); -> trovato è un problema di inserimento, guarda in insert
+        System.out.println(s);
+ // TO BE COMPLETED (finito?) 
     }
  }
  //TestProgram
@@ -79,15 +120,22 @@ import java.io.BufferedReader;
                 int operation = Integer.parseInt(line[0]);
                 switch (operation) {
                     case 0:
- // TO BE COMPLETED 
+                    MyEntry min_entry = skipList.min();
+                        if(min_entry != null) System.out.println(min_entry);
+ // TO BE COMPLETED (finito?)
                         break;
                     case 1:
- // TO BE COMPLETED 
+                    skipList.removeMin();
+ // TO BE COMPLETED (finito?)
                         break;
                     case 2:
- // TO BE COMPLETED 
+                    int key = Integer.parseInt(line[1]);
+                    String value = line[2];
+                    skipList.insert(key, value);
+// TO BE COMPLETED (finito?)
                         break;
                     case 3:
+                    skipList.print();
  // TO BE COMPLETED 
                         break;
                     default:
